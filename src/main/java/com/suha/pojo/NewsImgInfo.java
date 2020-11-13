fcec343eb8b4c40e68de56469e4cb70fcaa311c9
Administrator <18776187478@163.com>
2020-11-13 17:43:48 +0800

    实体类

--- src/main/java/com/suha/pojo/NewsImgInfo.java
+++ src/main/java/com/suha/pojo/NewsImgInfo.java
@@ -1,6 +1,19 @@
 package com.suha.pojo;
 
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Data
+@Table(name = "news_img_info")
 public class NewsImgInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;             //id（主键）
+    private Integer newsId;         //新闻序号
+    private String img;             //图片
 }
