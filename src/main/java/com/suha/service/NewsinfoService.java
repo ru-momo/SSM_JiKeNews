673f80cbe38b29a2a53e97ea294aec052cbdbb8e
HHHMiHu <18776187478@163.com>
2020-11-16 21:25:00 +0800

    1233

--- src/main/java/com/suha/service/NewsinfoService.java
+++ src/main/java/com/suha/service/NewsinfoService.java
@@ -1,5 +1,6 @@
 package com.suha.service;
 
+import com.suha.mapper.NewsInfoMapper;
 import com.suha.pojo.NewsInfo;
 import com.suha.util.Page;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +8,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class NewsinfoService extends BaseService<NewsInfo>{
-//    @Autowired
-//    private NewsinfoMapper nm;
-//    //重写父类的分页方法
-//   public Page<NewsInfo> getPageList(String name,Integer pageNum, Integer pageSize){
-//       return super.getPageList(nm,name,pageNum,pageSize);
-//   }
+    @Autowired
+    private NewsInfoMapper nm;
+    //重写父类的分页方法
+   public Page<NewsInfo> getPageList(String name,Integer pageNum, Integer pageSize){
+       return super.getPageList(nm,name,pageNum,pageSize);
+   }
 }
