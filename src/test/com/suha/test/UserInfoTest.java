cd52674706e193ccf387cb90327390f63fe47850
Administrator <18776187478@163.com>
2020-11-16 15:28:36 +0800

    用户管理

--- src/test/com/suha/test/UserInfoTest.java
+++ src/test/com/suha/test/UserInfoTest.java
@@ -1,8 +1,27 @@
 package com.suha.test;
-
-
+import com.suha.pojo.UserInfo;
+import com.suha.service.UserInfoService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/application-*.xml")
 public class UserInfoTest {
+    @Autowired
+    private UserInfoService us;
+
+    @Test
+    public void add(){
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUserName("HHH");
+        us.insertInfo(userInfo);
+        System.out.println(userInfo);
+    }
+
+
+
 }
+
