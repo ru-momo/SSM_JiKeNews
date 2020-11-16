86fca1a6350363923b8a3c2c1c6214793537d942
572233093@qq.com <572233093@qq.com>
2020-11-16 21:10:18 +0800

    fff

--- src/main/java/com/suha/controller/end/pagesAddDocController.java
+++ src/main/java/com/suha/controller/end/pagesAddDocController.java
@@ -5,12 +5,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class pagesAddDocController {
+
+    //接收表单数据
     @RequestMapping("end/pagesAddDoc")
     public String pagesAddDoc(){
 
-
-
-
         return "end/pagesAddDoc";
     }
+
+    //删除图片
+    @RequestMapping("end/pagesAddDoc/del")
+    public String del(){
+        return "";
+    }
+
+    // 看图片
+    @RequestMapping("end/pagesAddDoc/see")
+    public String see(){
+        return "";
+    }
 }
+
