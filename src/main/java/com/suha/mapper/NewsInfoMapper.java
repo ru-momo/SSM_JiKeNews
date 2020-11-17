f6a91c977b2f2c47a1df13824700425cb4f4efb1
panwenx_glb <panwenx_glb@shi.com>
2020-11-17 21:16:40 +0800

    pwx

--- src/main/java/com/suha/mapper/NewsInfoMapper.java
+++ src/main/java/com/suha/mapper/NewsInfoMapper.java
@@ -3,6 +3,12 @@ package com.suha.mapper;
 import com.suha.pojo.NewsInfo;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.List;
+import java.util.Map;
+
 public interface NewsInfoMapper extends Mapper<NewsInfo> {
 
+    List<NewsInfo> getListInfoByPage(Map<String,Object> map);
+
+    Integer getListCountByPage(Map<String,Object> map);
 }
